a6ee9de7996700fb94264a4948ec0859ed5483f4
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-06 23:01:01 -0700

    Added output logic
    If the key exists, display value, also fixed map names.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(){
         {"CM241", 1411}
     };
 
-    std::map <std::string, std::string> courseRoom{
+    std::map <std::string, std::string> courseInstructor{
         {"CS101", "Haynes"},
         {"CS102", "Alvarado"},
         {"CS103", "Rich"},
@@ -18,11 +18,24 @@ int main(){
         {"CM241", "Lee"}
     };
 
-    std::map <std::string, std::string> courseRoom{
+    std::map <std::string, std::string> courseTime{
         {"CS101", "8:00 a.m."},
         {"CS102", "9:00 a.m."},
         {"CS103", "10:00 a.m."},
         {"NT110", "11:00 a.m."},
         {"CM241", "1:00 p.m."}
     };
+
+    std::string userIn = "";
+    std::cin >> userIn;
+
+    if(courseRoom.count(userIn)){
+        std::cout << courseRoom.at(userIn) << std::endl;
+        std::cout << courseInstructor.at(userIn) << std::endl;
+        std::cout << courseTime.at(userIn) << std::endl;
+    }
+    else
+        std::cout << "Course not found." << std::endl;
+
+    return 0;
 }
